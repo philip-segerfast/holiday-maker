ad2561d72182dcbf7b39f87ac0ee359be740b1c4
Alex <kotzspam@gmail.com>
2021-05-03 11:40:42 +0200

    Created UserService

---
+++ src/main/java/newton/grupp2/holidaymaker/services/UserService.java
@@ -0,0 +1,15 @@
+package newton.grupp2.holidaymaker.services;
+
+import newton.grupp2.holidaymaker.entities.User;
+import newton.grupp2.holidaymaker.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+}
