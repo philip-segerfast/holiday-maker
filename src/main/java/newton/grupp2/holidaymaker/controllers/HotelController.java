4c67e2dc70fe575794c78ee8c979a011ef79a4d6
Peter <p.akesson@gmail.com>
2021-05-03 15:34:57 +0200

    added getAllHotels controller/service

--- src/main/java/newton/grupp2/holidaymaker/controllers/HotelController.java
+++ src/main/java/newton/grupp2/holidaymaker/controllers/HotelController.java
@@ -1,7 +1,23 @@
 package newton.grupp2.holidaymaker.controllers;
 
+import newton.grupp2.holidaymaker.entities.Hotel;
+import newton.grupp2.holidaymaker.services.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HotelController {
+    @Autowired
+    private HotelService hotelService;
+
+    //Get list of all hotels in database
+    @GetMapping("/rest/allhotels")
+    public List<Hotel> getAllHotels() {
+        return hotelService.getAllHotels();
+}
+
+
 }
