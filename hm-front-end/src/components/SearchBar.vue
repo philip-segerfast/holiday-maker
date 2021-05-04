a979b5ff5749a6f5e2c0db4b8b29534456038915
FCO <franne_@hotmail.com>
2021-05-04 11:44:24 +0200

    Added SearchBar.vue + searchbar logic in store.js (Not done)

---
+++ hm-front-end/src/components/SearchBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <form @submit.prevent="searchMethod">
+<input type="text" id="search" placeholder="type search" v-model="searchtext">
+<button type="submit">Search</button>
+</form>
+</template>
+
+<script>
+//FÖR SÖKRESULTAT
+import HotelCard from "../components/HotelCard.vue";
+
+export default {
+    data() {
+        return {
+            searchtext: ""
+        }
+    },
+
+    //Kolla upp "fetchHotels"
+    methods: {
+        searchMethod() {
+            let searchtext = this.searchtext
+            console.log("searchtext is: "+searchtext);
+            this.$store.commit("setSearchQuery", searchtext)
+            this.$store.dispatch("fetchHotels");
+        }
+    },
+
+    components: {
+        HotelCard,
+    },
+
+    computed: {
+        hotels(){
+            return this.$store.getters.getHotel
+        },
+
+    },
+    mounted() {
+        this.$store.dispatch("fetchHotel")
+    }
+
+};
+</script>
+
+<style>
+
+</style>
--- hm-front-end/src/store/store.js
+++ hm-front-end/src/store/store.js
@@ -4,10 +4,17 @@ import { createStore } from "vuex";
 export default createStore({
   state: {
     hotels: [],
+    searchQuery: "",
+
   },
   mutations: {
     setHotel(state, payload){
     state.hotels = payload;
+    },
+
+    //Hur implementeras detta??
+    setSearchQuery(state, payload) {
+      state.searchQuery = payload;
     },
   },
   actions: {
@@ -20,10 +27,26 @@ export default createStore({
         console.log(response.data)
       })
     },
+
+    //FÖR SÖKRESULTAT Kolla upp!
+    async fetchHotels() {
+      console.log('SearchQuery is: '+this.state.searchQuery)
+      const url ='http://localhost:3001/rest/programs/' + this.state.searchQuery
+      //const url ='http://localhost:3000/rest/programs/favorites'
+      await axios.get(url)
+      .then(response => {
+       this.commit("setHotels", response.data) 
+      })
+    },
   },
 
     getters: {
       getHotel(state){
+        return state.hotels
+      },
+
+      //SÖKRESULTAT KOLLA UPP!
+      getHotels(state){
         return state.hotels
       },
     }
