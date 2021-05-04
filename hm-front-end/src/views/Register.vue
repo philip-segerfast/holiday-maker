f3594a2634fb0090a9d655cdb1c2c8eb870e0ee8
Bilal Allouhaibi <bilalzmhaa99@gmail.com>
2021-05-04 10:06:28 +0200

    vue template register
    skaffa text input till password, email, first och last name, skaffa button

---
+++ hm-front-end/src/views/Register.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Register page</h1>
+    <input v v-model="email" type="text" placeholder="email" required>
+    <input v v-model="password" type="password" placeholder="passowrd" required>
+    <input type="text">
+    <input type="text">
+    <a type="button" @click="x">Register</a>
+</template>
+<script>
+
+export default {
+    setup() {
+        
+    },
+}
+</script>
