654c92f88f140a8c2e74cc0d9efded3796dbd679
Bilal Allouhaibi <bilalzmhaa99@gmail.com>
2021-05-06 13:32:32 +0200

    you can register but code isnt good
    you can register even if you dont fill the inputs have to force user to fill the input

--- hm-front-end/src/views/Register.vue
+++ hm-front-end/src/views/Register.vue
@@ -1,16 +1,32 @@
 <template>
-    <h1>Register page</h1>
-    <input v v-model="email" type="text" placeholder="Email" required>
-    <input v v-model="password" type="password" placeholder="Passowrd" required>
-    <input type="text" placeholder="First name">
-    <input type="text" placeholder="Last name">
-    <a type="button" @click="x">Register</a>
+  <h1>Register page</h1>
+  <input v-model="email" type="text" placeholder="email" required />
+  <input v-model="password" type="password" placeholder="password" required />
+  <input v-model="first_name" type="text" placeholder="First name" required />
+  <input v-model="last_name" type="text"  placeholder="Last name" required />
+  <button @click="register()">register</button>
 </template>
 <script>
-
 export default {
-    setup() {
-        
+  methods: {
+    async register() {
+     
+      let credentials = {
+        email: this.email,
+        password: this.password,
+        first_name: this.first_name,
+        last_name: this.last_name
+      };
+
+      let response = await fetch("/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(credentials),
+      });
+      console.log("i work" + response);
+    
+
     },
-}
+  },
+};
 </script>
