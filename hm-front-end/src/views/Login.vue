958c28b3f6d0b569dd67acf5e43ea4a0e35a61e7
Bilal Allouhaibi <bilalzmhaa99@gmail.com>
2021-05-07 11:39:44 +0200

    comments
    gave some codes comments to be more understandable

---
+++ hm-front-end/src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>
+      <!-- Hello -->
+      <div>LOGIN </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Login",
+  components: {},
+};
+</script>
--- hm-front-end/src/views/Register.vue
+++ hm-front-end/src/views/Register.vue
@@ -1,29 +1,33 @@
 <template>
   <h1>Register page</h1>
+  <!--skapar form så användaren kan mata in sina uppgifter -->
   <form>
     <input v-model="email" name="email" type="email" placeholder="email" required />
     <input v-model="password" type="password" placeholder="Password" required />
     <input v-model="first_name" type="text" placeholder="First name" required />
     <input v-model="last_name" type="text" placeholder="Last name" required />
     <button @click="register">
-      Submit
+      Register
     </button>
+    
   </form>
 </template>
 <script>
 export default {
+  //för route 
   name: "Register",
 
   methods: {
     async register() {
-      
+      //lägger de i credentails (objekt)
       let credentials = {
         email: this.email,
         password: this.password,
         first_name: this.first_name,
         last_name: this.last_name,
       };
-
+      //gör en POST request 
+      //lägger credentials i body
       let request = {
         method : "POST",
         headers: {
@@ -31,6 +35,7 @@ export default {
         },
         body: JSON.stringify(credentials)
       }
+      //fetchar request
       await fetch("/auth/register", request) 
  
       },
