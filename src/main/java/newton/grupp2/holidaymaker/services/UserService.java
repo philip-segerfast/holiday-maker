0c4c80b484778aac64407d42bc245f4d8dd80519
Bilal Allouhaibi <bilalzmhaa99@gmail.com>
2021-05-10 14:26:42 +0200

    Backend login is done
    you can login and get user by id and get all users in backend

--- src/main/java/newton/grupp2/holidaymaker/services/UserService.java
+++ src/main/java/newton/grupp2/holidaymaker/services/UserService.java
@@ -4,6 +4,7 @@ import newton.grupp2.holidaymaker.configurations.MyUserDetailsService;
 import newton.grupp2.holidaymaker.entities.User;
 import newton.grupp2.holidaymaker.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,9 +17,30 @@ public class UserService {
     @Autowired
     private MyUserDetailsService detailsService;
 
-    //for registering a user C 1.3
-    public User register(User user){
+    //for registering a user (C 1.3)
+    public User register(User user) {
         return detailsService.registerUser(user);
     }
 
+    //find logged in user (C.2)
+    public User whoAmI() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepository.findByEmail(email);
+    }
+
+    //get user by ID (C.2)
+    public User getById(long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            return user.get();
+        }
+        return null;
+    }
+    //get all users (C.2)
+    public List<User> getAll(){
+        return userRepository.findAll();
+    }
 }
+
+
+
