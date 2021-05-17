aed9ed9e2803ff8d24749ab1f2afc2253a46772c
Peter <p.akesson@gmail.com>
2021-05-17 09:17:26 +0200

    added bookingView

---
+++ hm-front-end/src/views/BookingView.vue
@@ -0,0 +1,23 @@
+<!-- 
+
+1. Component with - Hotel name and information (from Hotelpage?)
+2. Login/register-components
+3. List-component with bookingRoomCards-components
+4. Payment-component
+5. Price / confirm-booking-button
+
+-->
+
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
