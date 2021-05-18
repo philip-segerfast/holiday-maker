e9e30416455d466a8042df1a8735dd311b45fbc2
Peter <p.akesson@gmail.com>
2021-05-18 17:03:57 +0200

    small fixes

--- hm-front-end/src/components/BookingCard.vue
+++ hm-front-end/src/components/BookingCard.vue
@@ -1,10 +1,20 @@
 <template>
   <h3>Booking nr 1</h3>
+  <h4>{{ loggedInUser.email }}</h4>
+  <h4>{{ loggedInUser.bookings }}</h4>
+
 </template>
 
 <script>
 export default {
-
+computed: {
+    loggedInUser() {
+      return this.$store.state.loggedInUser
+    },
+    isLoggedIn() {
+      return this.loggedInUser != null
+    }
+  },
 }
 </script>
 
--- hm-front-end/src/components/bookingList.vue
+++ hm-front-end/src/components/bookingList.vue
@@ -1,6 +1,12 @@
 <template>
+  
+  <div v-if="isLoggedIn">
   <h2>List of bookings</h2>
   <BookingCard />
+  </div>
+  <div v-if ="!isLoggedIn">
+    <h2>Login to se booked hotels</h2>
+  </div>
 </template>
 
 <script>
@@ -8,6 +14,14 @@ import BookingCard from "./BookingCard"
 export default {
   components: { BookingCard },
 
+computed: {
+    loggedInUser() {
+      return this.$store.state.loggedInUser
+    },
+    isLoggedIn() {
+      return this.loggedInUser != null
+    }
+  },
 }
 </script>
 
