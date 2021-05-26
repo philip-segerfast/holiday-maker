dd8e4d6d844b92a74fff0598399814bdc7baa891
FCO <franne_@hotmail.com>
2021-05-26 14:49:23 +0200

    added ending </div>

--- hm-front-end/src/views/ResultPage.vue
+++ hm-front-end/src/views/ResultPage.vue
@@ -10,16 +10,17 @@
         <button @click="sortHotelsByMaxPrice">Max Price</button>
       </h3>
     </div>
-  <div class="main-container">
-    <!-- Insert hotels in hotelsList (Nestlar componenten HotelCard i ResultPage)-->
-    <!-- Loopar ut (v-for) listan av hotel enskilt = Skapar varje "hotel" som en egen komponent-->
-    <div v-if="filteredHotels.length > 0">
-      <HotelCard
-        v-for="(hotel, i) in filteredHotels"
-        :key="hotel + i"
-        :hotel="hotel"
-        class="container"
-      />
+    <div class="main-container">
+      <!-- Insert hotels in hotelsList (Nestlar componenten HotelCard i ResultPage)-->
+      <!-- Loopar ut (v-for) listan av hotel enskilt = Skapar varje "hotel" som en egen komponent-->
+      <div v-if="filteredHotels.length > 0">
+        <HotelCard
+          v-for="(hotel, i) in filteredHotels"
+          :key="hotel + i"
+          :hotel="hotel"
+          class="container"
+        />
+      </div>
     </div>
   </div>
 </template>
