cc7352d695b653c4694b9a3d8f7c70ddce37d9b0
Peter <p.akesson@gmail.com>
2021-05-31 15:48:03 +0200

    added @Postmapping addPayment

--- src/main/java/newton/grupp2/holidaymaker/controllers/PaymentController.java
+++ src/main/java/newton/grupp2/holidaymaker/controllers/PaymentController.java
@@ -1,12 +1,21 @@
 package newton.grupp2.holidaymaker.controllers;
 
+import newton.grupp2.holidaymaker.entities.Payment;
 import newton.grupp2.holidaymaker.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaymentController {
     @Autowired
     private PaymentService paymentService;
+
+    @PostMapping("/rest/payments")
+    public Payment addPayment(@RequestBody Payment payment) {
+        return paymentService.addPayment(payment);
+    }
+
 
 }
--- src/main/java/newton/grupp2/holidaymaker/services/PaymentService.java
+++ src/main/java/newton/grupp2/holidaymaker/services/PaymentService.java
@@ -1,5 +1,6 @@
 package newton.grupp2.holidaymaker.services;
 
+import newton.grupp2.holidaymaker.entities.Payment;
 import newton.grupp2.holidaymaker.repositories.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,7 @@ public class PaymentService {
     @Autowired
     private PaymentRepository paymentRepository;
 
+    public Payment addPayment(Payment payment) {
+       return paymentRepository.save(payment);
+    }
 }
