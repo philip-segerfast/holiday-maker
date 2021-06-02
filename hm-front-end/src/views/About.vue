ef586f20cac7644d1f3c44ef3b42ba30ea7cd41b
Peter <p.akesson@gmail.com>
2021-06-02 09:10:40 +0200

    Vue-stripe checkout button hardcoded

--- hm-front-end/src/views/About.vue
+++ hm-front-end/src/views/About.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <stripe-checkout
+      ref="checkoutRef"
+      mode="payment"
+      :pk="publishableKey"
+      :line-items="lineItems"
+      :success-url="successURL"
+      :cancel-url="cancelURL"
+      @loading="(v) => (loading = v)"
+    />
+    <button @click="submit">Pay now!</button>
   </div>
 </template>
+
+<script>
+import { StripeCheckout } from "@vue-stripe/vue-stripe";
+export default {
+  components: {
+    StripeCheckout,
+  },
+  data() {
+    this.publishableKey =
+      "pk_test_51IxU9qEuj6pxFvwiy7str1sRLwz43ylGHLiXkRzZuLVr08JZtnNYMbzzp27tQinEA5i8qXHOvAO0PBTEM1zBUpLT00L9DdTCNU";
+    return {
+      loading: false,
+      lineItems: [
+        {
+          //name: "TestHotel",
+          price: "price_1IxWwoEuj6pxFvwi0qiE8sf3", // The id of the one-time price you created in your Stripe dashboard
+          quantity: 1,
+        },
+      ],
+      successURL: "http://localhost:3000/",
+      cancelURL: "http://localhost:3000/about",
+    };
+  },
+  methods: {
+    submit() {
+      // You will be redirected to Stripe's secure checkout page
+      this.$refs.checkoutRef.redirectToCheckout();
+    },
+  },
+};
+</script>
