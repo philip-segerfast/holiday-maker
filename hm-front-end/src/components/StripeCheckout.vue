a6272436ba1dfa466ef0bebdf06f417c42e39e2b
Peter <p.akesson@gmail.com>
2021-06-04 09:27:10 +0200

    moved checkout to component

---
+++ hm-front-end/src/components/StripeCheckout.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <form @submit.prevent="toStripeCheckout">
+      <button type="submit">Confrim booking</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sessionId: null,
+      hotelName: "Sunny Side Hotel",
+      totalCost: 300,
+    };
+  },
+  computed: {},
+  methods: {
+    //This function sends us to the stripe checkout
+    async toStripeCheckout() {
+      var stripe = Stripe(
+        "pk_test_51IxU9qEuj6pxFvwiy7str1sRLwz43ylGHLiXkRzZuLVr08JZtnNYMbzzp27tQinEA5i8qXHOvAO0PBTEM1zBUpLT00L9DdTCNU"
+      );
+
+      let payload = {
+        hotelName: this.hotelName,
+        totalCost: this.totalCost,
+      };
+
+      await fetch("/rest/bookings/stripe/session", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (session) {
+          return stripe.redirectToCheckout({ sessionId: session.id });
+        })
+        .then(function (result) {
+          // If `redirectToCheckout` fails due to a browser or network
+          // error, you should display the localized error message to your
+          // customer using `error.message`.
+          if (result.error) {
+            alert(result.error.message);
+          }
+        });
+    },
+  },
+};
+</script>
--- hm-front-end/src/views/About.vue
+++ hm-front-end/src/views/About.vue
@@ -1,53 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <button @click="toStripeCheckout">To Stripe checkout</button>
+    <StripeCheckout />
   </div>
 </template>
 
 <script>
+import StripeCheckout from "../components/StripeCheckout.vue";
 export default {
-  data() {
-    return {
-      sessionId: null,
-      hotelName: "Sunny Side Hotel",
-      totalCost: 300,
-    };
-  },
-  methods: {
-    //This function sends us to the stripe checkout
-    async toStripeCheckout() {
-      var stripe = Stripe(
-        "pk_test_51IxU9qEuj6pxFvwiy7str1sRLwz43ylGHLiXkRzZuLVr08JZtnNYMbzzp27tQinEA5i8qXHOvAO0PBTEM1zBUpLT00L9DdTCNU"
-      );
-
-      let payload = {
-        hotelName: this.hotelName,
-        totalCost: this.totalCost,
-      };
-
-      await fetch("/rest/bookings/stripe/session", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(payload),
-      })
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (session) {
-          return stripe.redirectToCheckout({ sessionId: session.id });
-        })
-        .then(function (result) {
-          // If `redirectToCheckout` fails due to a browser or network
-          // error, you should display the localized error message to your
-          // customer using `error.message`.
-          if (result.error) {
-            alert(result.error.message);
-          }
-        });
-    },
-  },
+  components: { StripeCheckout },
 };
 </script>
